543af23e5b8fbd2eeb2b804ad9dc73c56a5fef81
LeoSirius <is.li.xiaoyu@qq.com>
2020-05-27 14:42:43 +0800

    update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ function App() {
   return (
     <div className="App">
       <div class="container">
-        <h1 class="mt-4">Hi~</h1>
-        <h2 class="mb-5">I'm Leo Sirius, a senior markdown engineer :)</h2>
+        <h1 class="mt-4 maroon-text">Hi~</h1>
+        <h2 class="mb-5 maroon-text">I'm Leo Sirius, a senior markdown engineer :)</h2>
         <p class="mt-5 font-weight-bold">You can contact me through following ways</p>
         <ul>
           <li><span class="font-weight-bold">Emails: </span>is.li.xiaoyu@qq.com, leosirius666@gmail.com (choose one, both works)</li>
           <li><span class="font-weight-bold">Github: </span><a target="_blank" href="https://github.com/LeoSirius">https://github.com/LeoSirius</a></li>
         </ul>
         <div>
-          <h2>Projects</h2>
-          <div class="work-main-content">
+          <h2 className="maroon-text">Projects</h2>
+          <div class="work-content">
             <a href="https://leosirius.fun/" class="work-title">blog</a>
             <p>blog</p>
             <a href="https://github.com/LeoSirius/leetcode_solutions" class="work-title">leetcode刷题笔记</a>
@@ -28,9 +28,9 @@ function App() {
             <a href="https://github.com/LeoSirius/code_beginning_linux_programming" class="work-title">linux程序设计 - 全书代码</a>
             <p>持续更新中...</p>
           </div>
-          <div class="foot-content">
-            <h5>关于本站</h5>
-            <a href="https://github.com/LeoSirius/react-gh-pages" class="foot-title">本站源码</a>
+          <div class="footer">
+            <h5 className="maroon-text">关于本站</h5>
+            <a href="https://github.com/LeoSirius/react-gh-pages" class="footer-title">本站源码</a>
             <p>本站是我的个人导航站。本站是用react写的静态网站，用gh-pages把编译后的代码托管到github pages仓库里</p>
           </div>
         </div>
