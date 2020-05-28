05eb05b76afe8a973c40cbbb8a941745d19ed1f6
LeoSirius <is.li.xiaoyu@qq.com>
2020-05-28 15:13:26 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './css/main.css';
 import './css/project-item.css';
 
+let goToUrl = (url) => {
+  window.open(url);
+}
+
 function ProjectItem(props) {
+
   return (
-    <div className="project-item">
-      <a className="project-title" href={props.url}>{props.name}</a>
+    <div className="project-item" onClick={goToUrl.bind(null, props.url)}>
+      <a className="project-title" href="#">{props.name}</a>
       <p className="project-desc">{props.desc}</p>
     </div>
   );
@@ -14,17 +19,17 @@ function ProjectItem(props) {
 function App() {
   return (
     <div className="App">
-      <div class="container">
-        <h1 class="mt-4 maroon-text">Hi~</h1>
-        <h2 class="mb-5 maroon-text">I'm Leo Sirius, a senior markdown engineer :)</h2>
-        <p class="mt-5 font-weight-bold">You can contact me through following ways</p>
+      <div className="container">
+        <h1 className="mt-4 maroon-text">Hi~</h1>
+        <h2 className="mb-5 maroon-text">I'm Leo Sirius, a senior markdown engineer :)</h2>
+        <p className="mt-5 font-weight-bold">You can contact me through following ways</p>
         <ul>
-          <li><span class="font-weight-bold">Emails: </span>is.li.xiaoyu@qq.com, leosirius666@gmail.com (choose one, both works)</li>
-          <li><span class="font-weight-bold">Github: </span><a target="_blank" href="https://github.com/LeoSirius">https://github.com/LeoSirius</a></li>
+          <li><span className="font-weight-bold">Emails: </span>is.li.xiaoyu@qq.com, leosirius666@gmail.com (choose one, both works)</li>
+          <li><span className="font-weight-bold">Github: </span><a target="_blank" href="https://github.com/LeoSirius">https://github.com/LeoSirius</a></li>
         </ul>
         <div>
           <h2 className="maroon-text">Projects</h2>
-          <div class="project-list">
+          <div className="project-list">
             <ProjectItem
               url={"https://leosirius.fun/"}
               name={"blog"}
@@ -37,23 +42,38 @@ function App() {
             />
             <ProjectItem
               url={"https://github.com/LeoSirius/code_apue"}
-              name={"apue - 全书代码"}
+              name={"apue - 代码"}
               desc={"持续更新中..."}
             />
             <ProjectItem
               url={"https://github.com/LeoSirius/code_unp"}
-              name={"unp - 全书代码"}
+              name={"unp - 代码"}
               desc={"持续更新中..."}
             />
             <ProjectItem
               url={"https://github.com/LeoSirius/code_beginning_linux_programming"}
-              name={"linux程序设计 - 全书代码"}
+              name={"linux程序设计 - 代码"}
+              desc={"持续更新中..."}
+            />
+            <ProjectItem
+              url={"https://github.com/LeoSirius/code_cpp_primer"}
+              name={"c++ primer - 代码"}
+              desc={"持续更新中..."}
+            />
+            <ProjectItem
+              url={"https://github.com/LeoSirius/code_linux_shell"}
+              name={"linux命令行与shell脚本编程大全 - 代码"}
+              desc={"持续更新中..."}
+            />
+            <ProjectItem
+              url={"https://github.com/LeoSirius/code_js_for_web_developers"}
+              name={"js高级程序设计 - 代码"}
               desc={"持续更新中..."}
             />
           </div>
-          <div class="footer">
+          <div className="footer">
             <h5 className="maroon-text">关于本站</h5>
-            <a href="https://github.com/LeoSirius/react-gh-pages" class="footer-title">本站源码</a>
+            <a href="https://github.com/LeoSirius/react-gh-pages" className="footer-title">本站源码</a>
             <p>本站是我的个人导航站。本站是用react写的静态网站，用gh-pages把编译后的代码托管到github pages仓库里</p>
           </div>
         </div>
